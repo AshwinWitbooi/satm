da487298f7e152426ddc256b19f0be901dc3c17a
Ashwin <ashwin.witbooi@gmail.com>
2026-09-24 21:02:14 +0200

    new project initial commit

---
+++ src/main/java/za/co/ashtech/samt/SamtApplication.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.samt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamtApplication.class, args);
+	}
+
+}
